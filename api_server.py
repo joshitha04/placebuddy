0c35bc18267ecadd55b09569ff37bb906ccc484c
Jv Shanmuga Teja <shanmuga2910@gmail.com>
2025-09-20 22:55:52 +0530

    requirements.txt

--- api_server.py
+++ api_server.py
@@ -20,6 +20,9 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = SQLAlchemy(app)
 
 
+Class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5501)
